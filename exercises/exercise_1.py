652b82a93c2f8f1908d66ddfb283435dfb7557cd
AndreaTheSigma <75577044+AndreaTheSigma@users.noreply.github.com>
2024-05-09 10:01:21 +0000

    solution

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,8 @@
 # Exercise 1
 # Your solution comes here
+number = int(input("Enter a 5 digit number: "))
+ones = number % 10
+hundreds = number // 100
+ten_thousands = (number // 10000) % 10
+sum_of_digits = ones + hundreds + ten_thousands
+print(f"The sum of the 1st, 3rd and 5th numbers is {sum_of_digits}")
--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,6 @@
 # Exercise 2
 # Your solution comes here
+num1=int(input("GIVE A NUMBER- A WHOLE INTEGER:       "))
+num2 = num1 + 1
+num3 = num1 + 2
+if num // 
