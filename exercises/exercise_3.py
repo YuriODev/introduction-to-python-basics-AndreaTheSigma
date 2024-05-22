c8798610eff7bef90aa58e8188f3e7d9a6696afe
AndreaTheSigma <75577044+AndreaTheSigma@users.noreply.github.com>
2024-05-22 12:40:40 +0000

    exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,6 +1,6 @@
 # Exercise 3
 # Your solution comes here
-um = int(input("Enter a number of seconds:   "))
+num = int(input("Enter a number of seconds:   "))
 hours = (num // 3600) % 24
 minutes = (num // 60) % 60
 seconds = num % 60
