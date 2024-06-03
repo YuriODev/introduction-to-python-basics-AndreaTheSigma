0b70b12059c4ae4056d8204ae8a4bf3a58d13081
AndreaTheSigma <75577044+AndreaTheSigma@users.noreply.github.com>
2024-06-03 08:54:32 +0000

    exercise 7

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,9 @@
 # Exercise 7
 # Your solution comes here
+num = int(input("Enter a 4 digit number: "))
+ones = num % 10
+tens = (num // 10) % 10
+hundreds = (num // 100) % 10
+thousands = num // 1000
+output = ones + tens + hundreds + thousands
+print(output)
